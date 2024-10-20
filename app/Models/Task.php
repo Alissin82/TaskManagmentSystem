3660d784cf5017999854b3c7ca807c3b3207e446
Alissin82 <92631352+Alissin82@users.noreply.github.com>
2024-10-20 10:09:06 +0330

    ***

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,11 +3,6 @@
 namespace App\Models;
 
 use App\Enums\TaskPriority;
-use App\Events\Task\TaskCreated;
-use App\Events\Task\TaskDeleted;
-use App\Events\Task\TaskImportantUpdated;
-use App\Events\Task\TasksHasChanges;
-use App\Events\Task\TaskUpdated;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -22,25 +17,4 @@ class Task extends Model
         'priority',
         'status',
     ];
-
-    protected $dispatchesEvents = [
-        'created' => [TaskCreated::class, TasksHasChanges::class],
-        'updated' => [TaskUpdated::class, TasksHasChanges::class],
-        'deleted' => [TaskDeleted::class, TasksHasChanges::class],
-    ];
-
-    protected static function booted(): void
-    {
-        static::updated(function ($task){
-            if($task->priority == TaskPriority::important->value && $task->wasChanged('priority') ){
-                event(new TaskImportantUpdated($task));
-            }
-        });
-
-        static::created(function ($task){
-            if($task->priority == TaskPriority::important->value ){
-                event(new TaskImportantUpdated($task));
-            }
-        });
-    }
 }
