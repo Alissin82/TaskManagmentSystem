d2f81e28b5ba8ef35e74e0abab0e670cc699038c
Alissin82 <92631352+Alissin82@users.noreply.github.com>
2024-10-27 20:38:35 +0330

    ***

--- resources/views/livewire/pages/tasks.blade.php
+++ resources/views/livewire/pages/tasks.blade.php
@@ -79,119 +79,124 @@
         </form>
     </div>
 
-    <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2">
-        @if( $tasks->isEmpty() )
-            <div class="md:col-span-2 lg:col-span-3 xl:col-span-4 bg-danger rounded p-1 py-3 text-center">
-                وظیفه‌ای وجود ندارد
-            </div>
-        @else
-            @foreach( $tasks as $task )
-                <div class="flex flex-col rounded-lg shadow border border-solid border-black p-2 gap-4 @if($task->id == $task_id) bg-gray-200 @endif">
-                    {{-- header --}}
-                    <div class="flex justify-between border-b pb-2 border-b-black">
-                        <div class="gap-2">
-                            <button
-                                type="button"
-                                class="btn bg-info"
-                                wire:click.prevent="patchStatus({{$task->id}}, '{{ TaskStatus::pending->value }}')">
-                                فعال
-                            </button>
-                            <button
-                                type="button"
-                                class="btn bg-danger"
-                                wire:click.prevent="patchStatus({{$task->id}}, '{{ TaskStatus::suspended->value }}')">
-                                تعویق
-                            </button>
-                            <button
-                                type="button"
-                                class="btn bg-success"
-                                wire:click.prevent="patchStatus({{$task->id}}, '{{ TaskStatus::completed->value }}')">
-                                تکمیل
-                            </button>
-                        </div>
-                        <div class="gap-2">
-                            <button type="button" wire:click.prevent="edit({{$task->id}})" class="btn bg-warning"> ویرایش </button>
-                            <button type="button" wire:click.prevent="delete({{$task->id}})" class="btn bg-danger"> حذف </button>
-                        </div>
-                    </div>
-
-                    {{-- body --}}
-                    <div class="flex flex-col gap-4">
-                        <div class="w-full flex justify-between">
-                            <div class="flex flex-col">
-                                <strong>وضعیت</strong>
-                                <x-inline.task-status :status="$task->status"/>
+    <div>
+        <h1 class="text-3xl mb-5">
+            وظایف
+        </h1>
+        <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2">
+            @if( $tasks->isEmpty() )
+                <div class="md:col-span-2 lg:col-span-3 xl:col-span-4 bg-danger rounded p-1 py-3 text-center">
+                    وظیفه‌ای وجود ندارد
+                </div>
+            @else
+                @foreach( $tasks as $task )
+                    <div class="flex flex-col rounded-lg shadow border border-solid border-black p-2 gap-4 @if($task->id == $task_id) bg-gray-200 @endif">
+                        {{-- header --}}
+                        <div class="flex justify-between border-b pb-2 border-b-black">
+                            <div class="gap-2">
+                                <button
+                                    type="button"
+                                    class="btn bg-info"
+                                    wire:click.prevent="patchStatus({{$task->id}}, '{{ TaskStatus::pending->value }}')">
+                                    فعال
+                                </button>
+                                <button
+                                    type="button"
+                                    class="btn bg-danger"
+                                    wire:click.prevent="patchStatus({{$task->id}}, '{{ TaskStatus::suspended->value }}')">
+                                    تعویق
+                                </button>
+                                <button
+                                    type="button"
+                                    class="btn bg-success"
+                                    wire:click.prevent="patchStatus({{$task->id}}, '{{ TaskStatus::completed->value }}')">
+                                    تکمیل
+                                </button>
                             </div>
-                            <div class="flex flex-col">
-                                <strong>اولویت</strong>
-                                <x-inline.task-priority :status="$task->priority"/>
+                            <div class="gap-2">
+                                <button type="button" wire:click.prevent="edit({{$task->id}})" class="btn bg-warning"> ویرایش </button>
+                                <button type="button" wire:click.prevent="delete({{$task->id}})" class="btn bg-danger"> حذف </button>
                             </div>
                         </div>
 
-                        <div class="flex justify-between">
-                            <div>
-                                <strong>عنوان</strong>
-                                <div>{{ $task->title }}</div>
+                        {{-- body --}}
+                        <div class="flex flex-col gap-4">
+                            <div class="w-full flex justify-between">
+                                <div class="flex flex-col">
+                                    <strong>وضعیت</strong>
+                                    <x-inline.task-status :status="$task->status"/>
+                                </div>
+                                <div class="flex flex-col">
+                                    <strong>اولویت</strong>
+                                    <x-inline.task-priority :status="$task->priority"/>
+                                </div>
                             </div>
-                            <div>
-                                <strong>تاریخ پایان</strong>
-                                <div class="flex w-full justify-end">
-                                    @if( $task->due_date != null)
-                                        @if( \Carbon\Carbon::hasFormat($task->due_date, 'Y-m-d H:i:s') )
-                                            <div class="flex flex-col items-end">
-                                                <x-inline.datetime :datetime="$task->due_date" format="Y/m/d" />
-                                                <x-inline.datetime :datetime="$task->due_date" format="H:i:s" />
-                                            </div>
-                                        @elseif( \Carbon\Carbon::hasFormat($task->due_date, 'Y-m-d') )
-                                            <div class="flex flex-row-reverse gap-2">
-                                                <x-inline.datetime :datetime="$task->due_date" format="Y/m/d" />
-                                            </div>
+
+                            <div class="flex justify-between">
+                                <div>
+                                    <strong>عنوان</strong>
+                                    <div>{{ $task->title }}</div>
+                                </div>
+                                <div>
+                                    <strong>تاریخ پایان</strong>
+                                    <div class="flex w-full justify-end">
+                                        @if( $task->due_date != null)
+                                            @if( \Carbon\Carbon::hasFormat($task->due_date, 'Y-m-d H:i:s') )
+                                                <div class="flex flex-col items-end">
+                                                    <x-inline.datetime :datetime="$task->due_date" format="Y/m/d" />
+                                                    <x-inline.datetime :datetime="$task->due_date" format="H:i:s" />
+                                                </div>
+                                            @elseif( \Carbon\Carbon::hasFormat($task->due_date, 'Y-m-d') )
+                                                <div class="flex flex-row-reverse gap-2">
+                                                    <x-inline.datetime :datetime="$task->due_date" format="Y/m/d" />
+                                                </div>
+                                            @else
+                                                <div>
+                                                    تاریخ نامعتبر
+                                                </div>
+                                            @endif
                                         @else
                                             <div>
-                                                تاریخ نامعتبر
+                                                بدون تاریخ
                                             </div>
                                         @endif
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div>
+                                <strong>توضیحات</strong>
+                                <div class="line-clamp-1">
+                                    @if( $task->description != null )
+                                        {{ $task->description }}
                                     @else
-                                        <div>
-                                            بدون تاریخ
-                                        </div>
+                                        بدون توضیحات
                                     @endif
                                 </div>
                             </div>
                         </div>
 
-                        <div>
-                            <strong>توضیحات</strong>
-                            <div class="line-clamp-1">
-                                @if( $task->description != null )
-                                    {{ $task->description }}
-                                @else
-                                    بدون توضیحات
-                                @endif
+                        {{-- footer --}}
+                        <div class="flex justify-between border-t pt-2 border-t-black">
+                            <div>
+                                <strong>تاریخ ساخت</strong>
+                                <div class="flex flex-row-reverse gap-2">
+                                    <x-inline.datetime :datetime="$task->created_at" format="Y/m/d" />
+                                    <x-inline.datetime :datetime="$task->created_at" format="H:i:s" />
+                                </div>
+                            </div>
+                            <div>
+                                <strong>تاریخ آخرین ویرایش</strong>
+                                <div class="flex flex-row-reverse gap-2">
+                                    <x-inline.datetime :datetime="$task->updated_at" format="Y/m/d" />
+                                    <x-inline.datetime :datetime="$task->updated_at" format="H:i:s" />
+                                </div>
                             </div>
                         </div>
                     </div>
-
-                    {{-- footer --}}
-                    <div class="flex justify-between border-t pt-2 border-t-black">
-                        <div>
-                            <strong>تاریخ ساخت</strong>
-                            <div class="flex flex-row-reverse gap-2">
-                                <x-inline.datetime :datetime="$task->created_at" format="Y/m/d" />
-                                <x-inline.datetime :datetime="$task->created_at" format="H:i:s" />
-                            </div>
-                        </div>
-                        <div>
-                            <strong>تاریخ آخرین ویرایش</strong>
-                            <div class="flex flex-row-reverse gap-2">
-                                <x-inline.datetime :datetime="$task->updated_at" format="Y/m/d" />
-                                <x-inline.datetime :datetime="$task->updated_at" format="H:i:s" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            @endforeach
-        @endif
+                @endforeach
+            @endif
+        </div>
     </div>
 </div>
 
